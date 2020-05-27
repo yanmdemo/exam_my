fa43b71150b987f6166768c71bf61e127df3adb4
YanM <yanm_eng@foxmail.com>
2020-05-27 14:05:29 +0800

    添加的修改内容

---
+++ src/main/java/com/exam_ym/Demo.java
@@ -0,0 +1,14 @@
+package com.exam_ym;
+
+/**
+ * @ClassName Demo
+ * @Description: TODO
+ * @Author YanM
+ * @Date 2020/5/27
+ * @Version V1.0
+ */
+public class Demo {
+    public static void get(){
+        System.out.println("nihoa");
+    }
+}
