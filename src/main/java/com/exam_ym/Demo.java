4ec6c1be977fd77a6a1238149da3d685ce8bfadb
YanM <yanm_eng@foxmail.com>
2020-05-27 19:05:26 +0800

    客户端修改

--- src/main/java/com/exam_ym/Demo.java
+++ src/main/java/com/exam_ym/Demo.java
@@ -10,6 +10,6 @@ package com.exam_ym;
 public class Demo {
     public static void get(){
         System.out.println("nihoa");
-        System.out.println("nihoa");
+        System.out.println("hahah");
     }
 }
