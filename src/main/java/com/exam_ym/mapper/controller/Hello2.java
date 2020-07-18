105b39d9c420d38c086de24371ea3ec5008383d9
yan_meng <yanm_eng@foxmail.com>
2020-07-18 08:50:23 +0800

    2020/07/18提交

--- src/main/java/com/exam_ym/mapper/controller/Hello2.java
+++ src/main/java/com/exam_ym/mapper/controller/Hello2.java
@@ -3,5 +3,6 @@ package com.exam_ym.mapper.controller;
 public class Hello2 {
     public static void main(String[] args) {
         System.out.println("另一个客户提交");
+        System.out.println("第二个用户");
     }
 }
